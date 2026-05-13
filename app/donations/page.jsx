3eb5bfffcb835487262109f79469df9aa3c792ab
Destrabe20 <144881086+Destrabe@users.noreply.github.com>
2026-05-13 18:51:56 +0000

    Donations Page

---
+++ app/donations/page.jsx
@@ -0,0 +1,9 @@
+import "./donations.css"; 
+
+export default function DonationsPage() {
+  return (
+    <div className="container">
+      <h1 className="title">Próximamente</h1>
+    </div>
+  );
+}
